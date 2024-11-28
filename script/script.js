eb38ce6a481062c5ebc1ae697e8c222b20e18f39
Debugger <mbiydzenyuyeileen@gmail.com>
2024-11-28 23:46:53 +0100

    commiting javascript logic added to the calculator phone app

--- script/script.js
+++ script/script.js
@@ -3,24 +3,20 @@ const buttons = document.querySelectorAll("button");
 
 let string = "";
 const arr = Array.from(buttons);
-console.log(arr);
 arr.forEach((button) => {
-  console.log(button);
-
   button.addEventListener("click", (e) => {
-    console.log(e);
-
-    if (e.target.innerHTML == "=") {
+    console.log({ string: e.target.innerHTML });
+    if (e.target.innerHTML.trim() == "=") {
       string = eval(string);
       input.value = string;
-    } else if (e.target.innerHTML == "AC") {
+    } else if (e.target.innerHTML.trim() == "AC") {
       string = "";
       input.value = string;
-    } else if (e.target.innerHTML == "DE") {
+    } else if (e.target.innerHTML.trim() == "DE") {
       string = string.substring(0, string.length - 1);
       input.value = string;
     } else {
-      string += e.target.innerHTML;
+      string += e.target.innerHTML.trim();
       input.value = string;
     }
   });
