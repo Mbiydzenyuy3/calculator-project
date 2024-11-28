1116be8fc43993460427e9577eb0b1717c58efc9
Debugger <mbiydzenyuyeileen@gmail.com>
2024-11-29 00:10:53 +0100

    commiting changes made to the equal to button to become functional

--- script/script.js
+++ script/script.js
@@ -1,23 +1,23 @@
-const input = document.getElementById("display");
-const buttons = document.querySelectorAll("button");
+const input = document.getElementById('display')
+const buttons = document.querySelectorAll('button')
 
-let string = "";
-const arr = Array.from(buttons);
+let string = ''
+const arr = Array.from(buttons)
 arr.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    console.log({ string: e.target.innerHTML });
-    if (e.target.innerHTML.trim() == "=") {
-      string = eval(string);
-      input.value = string;
-    } else if (e.target.innerHTML.trim() == "AC") {
-      string = "";
-      input.value = string;
-    } else if (e.target.innerHTML.trim() == "DE") {
-      string = string.substring(0, string.length - 1);
-      input.value = string;
+  button.addEventListener('click', (e) => {
+    console.log({ string: e.target.innerHTML })
+    if (e.target.innerHTML.trim() == '=') {
+      string = eval(string)
+      input.value = string
+    } else if (e.target.innerHTML.trim() == 'AC') {
+      string = ''
+      input.value = string
+    } else if (e.target.innerHTML.trim() == 'DE') {
+      string = string.substring(0, string.length - 1)
+      input.value = string
     } else {
-      string += e.target.innerHTML.trim();
-      input.value = string;
+      string += e.target.innerHTML.trim()
+      input.value = string
     }
-  });
-});
+  })
+})
