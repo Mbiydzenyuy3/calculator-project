31d7bed8f852c42259dfec280317c66519340af9
Debugger <mbiydzenyuyeileen@gmail.com>
2024-11-29 06:39:55 +0100

    commiting changes made to the equal to button to become functional

--- script/script.js
+++ script/script.js
@@ -1,20 +1,22 @@
 const input = document.getElementById('display')
 const buttons = document.querySelectorAll('button')
 
+// const { eval } = window
+const E = eval
+
 let string = ''
 const arr = Array.from(buttons)
 arr.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    console.log({ string: e.target.innerHTML })
+  button.addEventListener('click', (e) => {
     if (e.target.innerHTML.trim() === '=') {
-      string = eval(input.value)
+      string = E(string)
       input.value = string
     } else if (e.target.innerHTML.trim() === 'AC') {
       string = ''
-      input.value = string; 
+      input.value = string
     } else if (e.target.innerHTML.trim() === 'DE') {
       string = string.substring(0, string.length - 1)
-      input.value = string;
+      input.value = string
     } else {
       string += e.target.innerHTML.trim()
       input.value = string
