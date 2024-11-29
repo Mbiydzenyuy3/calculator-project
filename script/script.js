2284cd12a2f5531b947565e76ae9f9962ac220f0
Debugger <mbiydzenyuyeileen@gmail.com>
2024-11-29 01:35:52 +0100

    commiting changes made to the equal to button to become functional

--- script/script.js
+++ script/script.js
@@ -4,21 +4,17 @@ const buttons = document.querySelectorAll('button')
 let string = ''
 const arr = Array.from(buttons)
 arr.forEach((button) => {
-  button.addEventListener('click', (e) => {
+  button.addEventListener("click", (e) => {
     console.log({ string: e.target.innerHTML })
-    if (e.target.innerHTML.trim() == '=') {
-      try {
-        string = new Function('return' + string)()
-        input.value = string
-      } catch (error) {
-        input.value = 'Error'
-      }
-    } else if (e.target.innerHTML.trim() == 'AC') {
+    if (e.target.innerHTML.trim() === '=') {
+      string = eval(input.value)
+      input.value = string
+    } else if (e.target.innerHTML.trim() === 'AC') {
       string = ''
-      input.value = string
-    } else if (e.target.innerHTML.trim() == 'DE') {
+      input.value = string; 
+    } else if (e.target.innerHTML.trim() === 'DE') {
       string = string.substring(0, string.length - 1)
-      input.value = string
+      input.value = string;
     } else {
       string += e.target.innerHTML.trim()
       input.value = string
